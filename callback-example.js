2fe8255eb8a3a097832c01e10aec1f826bb1f89e
samuel21695 <samuel21695@gmail.com>
2023-10-05 09:44:22 +0900

    return a-b when calling (common callback method)

--- callback-example.js
+++ callback-example.js
@@ -9,3 +9,13 @@ function one(a, b, callback) {
 // 기존에 있던 함수를 콜백함수로 활용한 경우
 let testOne = one(1, 2, exampleCallback) 
 console.log(testOne);
+
+function two(a, b, callback) {
+  return callback(a, b);
+}
+
+// 호출할 때 함수를 작성해주는 경우 -> 일반적인 콜백함수 방식
+let testTwo = two(2, 3, function(a, b) {
+  return a - b;
+});
+console.log(testTwo);
