3d9932d6067ecc9eb2df299afc93ba68a2b4c3fa
samuel21695 <samuel21695@gmail.com>
2023-10-05 09:39:14 +0900

    implement existing function

--- callback-example.js
+++ callback-example.js
@@ -1,9 +1,11 @@
 function exampleCallback(a, b) {
-  console.log(a + b);
+  return(a + b);
 }
 
 function one(a, b, callback) {
   return callback(a, b);
 }
 
-one(1, 3, exampleCallback)
+// 기존에 있던 함수를 콜백함수로 활용한 경우
+let testOne = one(1, 2, exampleCallback) 
+console.log(testOne);
